f3efc9759ca000ad5e49ac7a6eb61c9f386f11c6
Thinh Le <54289320+heydayle@users.noreply.github.com>
2025-03-04 17:11:18 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
     app: {
         pageTransition: {name: 'page', mode: 'out-in'},
         head: {
-            meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+            meta: [
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: 'google-site-verification', content: 'f1RWMSTmyECrT0CoMRIa8tOBPIaaAsUiTT7qYLQ1mS0' }
+            ],
         }
     },
     css: ["@/assets/scss/app.scss"],
