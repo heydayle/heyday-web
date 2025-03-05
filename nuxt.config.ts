3df0e7d2a61dda621ec866aafef70fdf5d053674
Thinh Le <54289320+heydayle@users.noreply.github.com>
2025-03-05 04:37:51 +0000

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
-    app: {
-        pageTransition: {name: 'page', mode: 'out-in'},
-        head: {
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'google-site-verification', content: 'f1RWMSTmyECrT0CoMRIa8tOBPIaaAsUiTT7qYLQ1mS0' }
-            ],
-        }
-    },
-    css: ["@/assets/scss/app.scss"],
-    vite: {
-        server: {
-            hmr: {
-                port: 5050
-            }
-        }
-    },
-    supabase: {
-        url: process.env.SUPABASE_URL,
-        key: process.env.SUPABASE_KEY,
-        redirect: false
-    }
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', '@nuxt/icon'],
+
+  app: {
+      pageTransition: {name: 'page', mode: 'out-in'},
+      head: {
+          meta: [
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+              { name: 'google-site-verification', content: 'f1RWMSTmyECrT0CoMRIa8tOBPIaaAsUiTT7qYLQ1mS0' }
+          ],
+      }
+  },
+
+  css: ["@/assets/scss/app.scss"],
+
+  vite: {
+      server: {
+          hmr: {
+              port: 5050
+          }
+      }
+  },
+
+  supabase: {
+      url: process.env.SUPABASE_URL,
+      key: process.env.SUPABASE_KEY,
+      redirect: false
+  },
+
+  compatibilityDate: '2025-03-05'
 });
