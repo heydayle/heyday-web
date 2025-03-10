f43771d13939701285aa7ab02b54cc11af51556d
heyday <hungthinh.ckc@gmail.com>
2025-03-10 20:59:07 +0700

    change: remove supabase

--- composables/usePortfolio.js
+++ composables/usePortfolio.js
@@ -4,9 +4,6 @@ import aboutData from '@/assets/mock/about.json'
 export const usePortfolio = async (listedOnly = false) => {
     const portfolio = useState('portfolio', () => []);
     const nuxtConfig = useAppConfig()
-    
-    const client = useSupabaseClient()
-    const projects = useState('projects', () => projectsData)
 
     const getAllProjects = (slug) => {
         const data = (projectsData.find(e => e.slug === slug));
@@ -17,23 +14,16 @@ export const usePortfolio = async (listedOnly = false) => {
     }
 
     const getProjectBySlug = (slug) => {
-        console.log(slug);
-        console.log(projectsDetail);
-        
         return projectsDetail[slug]
-        // return client.from('contents').select().eq('slug', slug)
     }
     const getAbout = () => {
         return aboutData
-        // return client.from('about').select()
     }
 
     if (portfolio.value.length === 0) {
         portfolio.value = nuxtConfig?.projects;
     }
-    // if(listedOnly){
-    //     return ref(portfolio.value.filter(item => item["listed"]));
-    // }
+
     return {
         getAllProjects,
         getProjectBySlug,
